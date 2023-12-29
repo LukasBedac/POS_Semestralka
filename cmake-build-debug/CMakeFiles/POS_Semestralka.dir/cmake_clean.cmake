277fb7b11c4f6dbc77208410ff66ef4d27a8829d
Lukas Bedac <lukasbedac222@azet.sk>
2023-12-29 12:45:36 +0100

    Init commit

---
+++ cmake-build-debug/CMakeFiles/POS_Semestralka.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS_Semestralka.dir/main.cpp.o"
+  "POS_Semestralka"
+  "POS_Semestralka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POS_Semestralka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Lukas on 29. 12. 2023.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World" << std::endl;
+    return 0;
+}
