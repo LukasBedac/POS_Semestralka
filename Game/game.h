59130a45f6848bf0b7f867033cf6ee2e7576b11b
bexqth <nicollsprite@gmail.com>
2023-12-30 19:45:34 +0100

    game doplnenie

--- Game/game.h
+++ Game/game.h
@@ -2,14 +2,17 @@
 // Created by Lukas on 30. 12. 2023.
 //
 
+#include "HraciaPlocha.h"
+#include "Kocka.h"
+
 #ifndef POS_SEMESTRALKA_GAME_H
 #define POS_SEMESTRALKA_GAME_H
 
 
 class game{
-
 private:
-
+    HraciaPlocha hraciaPlocha;
+    Kocka kocka;
 public:
 
 };
